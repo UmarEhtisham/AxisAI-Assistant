edbd6e34033ead3ea6eb4eb6af2c6fc7e53c4933
mrqadeer <mrqadeer1231122@gmail.com>
2024-09-18 14:34:52 +0500

    working on UI/UX

--- views/credentials.py
+++ views/credentials.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.header("Credentials")
 st.write("Please provide your credentials. You can use any of the following:")
+submit=st.button("Submit")
+@st.experimental_dialog("Credentials")
+def credentials():
+    username=st.text_input("Username")
+credentials()
